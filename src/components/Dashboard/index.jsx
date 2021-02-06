ac35868ecece2562eab9b4857bb72e6fe74b2486
Francisco Guilherme <franciscoguilherme.mundo@outlook.com>
2021-02-06 18:35:21 -0300

    [master] - Implementa alerta sobre parada de processo.

--- src/components/Dashboard/index.jsx
+++ src/components/Dashboard/index.jsx
@@ -3,12 +3,16 @@ import {
   Grid,
   Card,
   Avatar,
+  Snackbar,
   Container,
+  IconButton,
   CardHeader,
   CardActionArea
 } from "@material-ui/core"
+import MuiAlert from "@material-ui/lab/Alert"
 import { useStyles } from "./assets/style"
 import People from "@material-ui/icons/People"
+import CloseIcon from "@material-ui/icons/Close"
 
 import Vendas from "./Vendas"
 
@@ -24,9 +28,19 @@ const client = new WebSocket(process.env.REACT_APP_MSGPI_WEBSOCKET)
 
 const Dashboard = () => {
   const classes = useStyles()
+  const [message, setMessage] = useState(false)
+  const [process, setProcess] = useState("")
   const loader = useContext(LoaderContext)
   const [ordersRows, setOrdersRows] = useState([])
   const [processRows, setProcessRows] = useState([])
+
+  function Alert(props) {
+    return <MuiAlert elevation={6} variant="filled" {...props} />;
+  }
+
+  const handleClose = () => {
+    setMessage(false)
+  }
 
   useEffect(() => {
     loader(true)
@@ -35,6 +49,8 @@ const Dashboard = () => {
       .then((response) => {
         loader(false)
         setProcessRows(getProcessRows(response[0]))
+
+        localStorage.setItem('process', JSON.stringify(getProcessRows(response[0])))
       })
       .catch((error) => {
         loader(false)
@@ -52,11 +68,42 @@ const Dashboard = () => {
       })
 
     client.onopen = () => { console.log('WebSocket Client Connected') }
-    client.onmessage = (message) => { console.log(message) }
+    client.onmessage = (message) => {
+      let updateProcessRows = JSON.parse(localStorage.getItem('process'))
+
+      const item = updateProcessRows.find(element => (element.numero === message.data))
+      const index = updateProcessRows.indexOf(item, 0)
+
+      updateProcessRows[index].status = "stopped"
+
+      setMessage(true)
+      setProcess(message.data)
+      setProcessRows(updateProcessRows)
+
+      client.close()
+      client.onopen()
+    }
   }, [])
 
   return (
     <Grid container>
+      <Snackbar
+        anchorOrigin={{ vertical: "top", horizontal: "right" }}
+        open={message}
+        key="topright"
+        action={
+          <React.Fragment>
+            <IconButton size="small" aria-label="close" color="inherit" onClick={handleClose}>
+              <CloseIcon fontSize="small" />
+            </IconButton>
+          </React.Fragment>
+        }
+      >
+        <Alert onClose={handleClose} severity="error">
+          Ocorreu uma parada no processo {process}!
+        </Alert>
+      </Snackbar>
+
       {modulesList.map((module, index) => (
         <Grid key={index} item md={6} xl={4} xs={12}>
           <Container className={classes.cardGrid} >
